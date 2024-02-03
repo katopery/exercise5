7549594c81c1721df55c63461d37d6f234b2bc5e
MasatoKato <152849350+katopery@users.noreply.github.co>
2024-02-03 12:15:48 +0000

    [Finish] 検索機能完成

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,15 @@
 class SearchesController < ApplicationController
-  
-  def serch
-  
+  before_action :authenticate_user!
+
+  def search
+    @range = params[:range]
+    @word = params[:word]
+    
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+    else
+      @books = Book.looks(params[:search], params[:word])
+    end
   end
   
 end
